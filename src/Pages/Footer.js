e55fdd71df00e0207acacb90c2734545a70620c2
zobayertihan <zobayertihan@gmail.com>
2023-05-17 23:27:17 +0600

    Footer page updated

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,39 +1,56 @@
 import React from 'react';
+import logo from '../Assets/DR-removebg-preview.png'
+import { HiLocationMarker } from "react-icons/hi";
+import { MdEmail } from 'react-icons/md';
+import { AiTwotonePhone } from 'react-icons/ai';
+
 
 const Footer = () => {
     return (
-        <div className='py-5'>
-            <div className='flex gap-5'>
-                <div>
-                    <h1>LOGO</h1>
-                    <h1>Address</h1>
-                    <h1>Phone</h1>
-                    <h1>Email</h1>
+        <div className='py-5 bg-[#3a4190] px-20'>
+            <div className='grid grid-cols-4 text-white py-20'>
+                <div className='flex flex-col justify-center items-start gap-2'>
+                    <img className='rounded p-1 h-24 w-48 bg-gray-50' src={logo} alt="" />
+                    <div className='w-3/4 flex gap-2 items-start'>
+                        <HiLocationMarker className='text-4xl' />
+                        <p>Bandhabari, Kaliakair, Gazipur 1750 Gazipur, Dhaka Division, Bangladesh</p>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <MdEmail />
+                        <h1>dhakadreamlandpark@gmail.com</h1>
+                    </div>
+                    <div className='flex items-center gap-2'>
+                        <AiTwotonePhone className='text-white' />
+                        <h1>+8801762-554444</h1>
+                    </div>
                 </div>
-                <div>
-                    <h1>Explore</h1>
+                <div className='flex flex-col justify-center items-start gap-5'>
+                    <h1 className='text-2xl font-bold'>Explore</h1>
                     <h1>About Us</h1>
                     <h1>Eco Park</h1>
                     <h1>Events</h1>
                     <h1>Packages</h1>
                 </div>
-                <div>
-                    <h1>Resources</h1>
+                <div className='flex flex-col justify-center items-start gap-5'>
+                    <h1 className='text-2xl font-bold'>Resources</h1>
                     <h1>FAQ's</h1>
                     <h1>Terms & Conditions</h1>
                     <h1>Privacy Policy</h1>
                     <h1>Contact Us</h1>
                 </div>
-                <div>
-                    <h1>Don't Miss A Thing</h1>
-                    <p>Get update to special deals and exclusive offers. Sign up to our newsletter!</p>
-                    <div>
-                        <input className='border rounded-lg' type="text" name="" id="" placeholder='Your Email Address' />
-                        <button className='rounded-full bg-black text-white px-2'>Subscribe</button>
+                <div className='flex flex-col justify-start items-start gap-5'>
+                    <h1 className='text-2xl font-bold pt-5'>Don't Miss A Thing</h1>
+                    <p className='w-3/4 pe-5'>Get update to special deals and exclusive offers. Sign up to our newsletter!</p>
+                    <div className='relative pt-8'>
+                        <input className='border rounded-full py-2 pe-20 ps-3' type="text" name="" id="" placeholder='Your Email Address' />
+                        <div className='absolute top-9 right-1'>
+                            <button className='rounded-full bg-[#3a4190] text-white text-sm font-bold px-3 py-2'>Subscribe</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <h1 className='text-center'>Copyright 2022 <span className='font-bold'>Dhaka Resort.</span> All rights reserved.</h1>
+            <hr className='px-20 py-2' />
+            <h1 className='text-center text-white'>Copyright 2022 <span className='font-bold'>Dhaka Resort.</span> All rights reserved.</h1>
         </div>
     );
 };
--- src/Pages/Offers.js
+++ src/Pages/Offers.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Offers = () => {
     return (
         <div className='bg-image3 mx-auto h-[200px] py-10'>
-            <h1 className='text-center text-white font-bold text-3xl p-10'>DISCOVER SPECIAL OFFERS</h1>
-            <p className='text-center text-white p-5'>Make sure to check out these special promotions</p>
+            <h1 className='text-center text-white font-bold text-4xl p-10'>DISCOVER SPECIAL OFFERS</h1>
+            <p className='text-center text-white pb-20'>Make sure to check out these special promotions</p>
             <div className='flex justify-center pb-20'>
                 <button className='rounded-full bg-white text-sm text-[#3a4190] px-8 py-4 font-bold'>VIEW ALL OFFERS</button>
             </div>
